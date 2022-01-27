e4cf1aa8ba7f6b6165c657dd9fad45fd84291a52
Brett Bowles <brettbowles@Bretts-MacBook-Pro.local>
2022-01-26 23:06:47 -0500

    come on baby

--- cryptoBots/settings.py
+++ cryptoBots/settings.py
@@ -30,9 +30,10 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
+    'localhost',
     '127.0.0.1',
     'https://crypto-bots-back.herokuapp.com'
     ]
