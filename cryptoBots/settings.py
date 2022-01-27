3eb79d367b3638fccbd9094d992cb59fb9097851
Brett Bowles <brettbowles@Bretts-MacBook-Pro.local>
2022-01-26 22:52:40 -0500

    changed settings

--- cryptoBots/settings.py
+++ cryptoBots/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = env('SECRET_KEY')
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://crypto-bots-back.herokuapp.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'https://crypto-bots-back.herokuapp.com'
+    ]
 
 
 # Application definition
