4939b86cd5cd29968265791d3c841fecd7d4f62d
Brett Bowles <brettbowles@Bretts-MacBook-Pro.local>
2022-01-26 23:22:51 -0500

    settings change

--- cryptoBots/settings.py
+++ cryptoBots/settings.py
@@ -29,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = [
-    # 'localhost',
-    # '127.0.0.1',
+    'localhost',
+    '127.0.0.1',
     'https://crypto-bots-back.herokuapp.com/'
     ]
 
