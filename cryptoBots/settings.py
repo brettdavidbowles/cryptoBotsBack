5c96a50449b7d18fde822a243d9e8ce75a61c50a
Brett Bowles <brettbowles@Bretts-MacBook-Pro.local>
2022-04-03 16:39:16 -0400

    added cors package

--- cryptoBots/settings.py
+++ cryptoBots/settings.py
@@ -50,18 +50,25 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:3000",
+"http://127.0.0.1:3000"
 ]
 
 
